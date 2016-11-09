632438ff59b3aea44d50427716e4911046b7b00f
samuel <samuel@sotechsaudecom.br>
2016-11-09 16:07:03 -0300

    new archives

--- expressoes-regulares/expressoes-regulares.js
+++ expressoes-regulares/expressoes-regulares.js
@@ -4,19 +4,49 @@ window.onload = function(){
 
 	document.querySelector("#btn-passo-1").addEventListener("click", function(e){
 		e.preventDefault();
-		validarTelefoneExec();
+		validarTelefoneExec(/99999-9999/, "telefone-passo-1");		
+	});
+
+	document.querySelector("#btn-passo-2").addEventListener("click", function(e){
+		e.preventDefault();
+		validarTelefoneTest(/\(48\) 99999-9999/, "telefone-passo-2");
+	});
+
+	document.querySelector("#btn-passo-3").addEventListener("click", function(e){
+		e.preventDefault();
+		validarTelefoneTest(/\(48\) 99999-9999/, "telefone-passo-3");
+	});
+
+	document.querySelector("#btn-passo-4").addEventListener("click", function(e){
+		e.preventDefault();
+		validarTelefoneTest(/^\([0-9][0-9]\) [0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$/, "telefone-passo-4");
+	});
+
+	document.querySelector("#btn-passo-5").addEventListener("click", function(e){
+		e.preventDefault();
+		validarTelefoneTest(/^\([0-9]{2}\) [0-9]{5}-[0-9]{4}$/, "telefone-passo-5");
+	});
+
+	document.querySelector("#btn-passo-6").addEventListener("click", function(e){
+		e.preventDefault();
+		validarTelefoneTest(/^\([0-9]{2}\) [0-9]{4,5}-[0-9]{4}$/, "telefone-passo-6");
+	});
+
+	document.querySelector("#btn-passo-7").addEventListener("click", function(e){
+		e.preventDefault();
+		validarTelefoneTest(/^\([0-9]{2}\) [0-9]{4,5}-?[0-9]{4}$/, "telefone-passo-7");
 	});
 
 }
 
 /*exec*/
-function validarTelefoneExec(){
-	var telefone = document.querySelector("#telefone-passo-1").value;
+function validarTelefoneExec(regExp, idComponente){
+	var telefone = document.querySelector("#"+idComponente).value;
 	console.log(regExp.exec(telefone));
 }
 
 /*test*/
-function validarTelefoneTest(){
-	var telefone = document.querySelector("#telefone-passo-1").value;
+function validarTelefoneTest(regExp, idComponente){
+	var telefone = document.querySelector("#"+idComponente).value;
 	console.log(regExp.test(telefone));
 }
