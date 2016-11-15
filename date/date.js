1a136e4e033dc11b3f6be0ff7d7b9ca0745b596e
samuel <samuelsilvawb@gmail.com>
2016-11-15 19:42:02 -0300

    starting to operators

--- date/date.js
+++ date/date.js
@@ -2,5 +2,6 @@ window.onload = function(){
 	document.getElementById("data").addEventListener('click',function(e){
 		e.preventDefault();
 		document.getElementById("resultado").innerHTML = new Date();
+		document.getElementById("resultado-time").innerHTML = new Date().getTime();
 	});
 }
