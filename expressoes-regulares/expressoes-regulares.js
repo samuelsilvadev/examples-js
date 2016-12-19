abbec49ac52a8a275e0aa78de6660b4ea151c384
samuel <samuel@sotechsaudecom.br>
2016-12-19 13:55:41 -0300

    refatorando validação de expressoes regulares

--- expressoes-regulares/expressoes-regulares.js
+++ expressoes-regulares/expressoes-regulares.js
@@ -1,47 +1,13 @@
 var regExp= /99999-9999/;
 
 window.onload = function(){
-
-	document.querySelector("#btn-passo-1").addEventListener("click", function(e){
+	document.querySelector("#valorDeTeste").value;
+	document.querySelector("#validaRegExp").addEventListener("click", function(e){
 		e.preventDefault();
-		validarTelefoneExec(/99999-9999/, "telefone-passo-1");		
+		var regExp = document.querySelector("#regExp").value;
+		console.log(regExp);
+		validarTelefoneTest(regExp, "valorDeTeste");
 	});
-
-	document.querySelector("#btn-passo-2").addEventListener("click", function(e){
-		e.preventDefault();
-		validarTelefoneTest(/\(48\) 99999-9999/, "telefone-passo-2");
-	});
-
-	document.querySelector("#btn-passo-3").addEventListener("click", function(e){
-		e.preventDefault();
-		validarTelefoneTest(/\(48\) 99999-9999/, "telefone-passo-3");
-	});
-
-	document.querySelector("#btn-passo-4").addEventListener("click", function(e){
-		e.preventDefault();
-		validarTelefoneTest(/^\([0-9][0-9]\) [0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$/, "telefone-passo-4");
-	});
-
-	document.querySelector("#btn-passo-5").addEventListener("click", function(e){
-		e.preventDefault();
-		validarTelefoneTest(/^\([0-9]{2}\) [0-9]{5}-[0-9]{4}$/, "telefone-passo-5");
-	});
-
-	document.querySelector("#btn-passo-6").addEventListener("click", function(e){
-		e.preventDefault();
-		validarTelefoneTest(/^\([0-9]{2}\) [0-9]{4,5}-[0-9]{4}$/, "telefone-passo-6");
-	});
-
-	document.querySelector("#btn-passo-7").addEventListener("click", function(e){
-		e.preventDefault();
-		validarTelefoneTest(/^\([0-9]{2}\) [0-9]{4,5}-?[0-9]{4}$/, "telefone-passo-7");
-	});
-
-	document.querySelector("#btn-passo-9").addEventListener("click", function(e){
-		e.preventDefault();
-		validarTelefoneTest(/^\(\d2}\)\s\d{4,5}-?\d{4}$/, "telefone-passo-9");
-	});
-
 }
 
 /*exec*/
@@ -53,5 +19,6 @@ function validarTelefoneExec(regExp, idComponente){
 /*test*/
 function validarTelefoneTest(regExp, idComponente){
 	var telefone = document.querySelector("#"+idComponente).value;
-	console.log(regExp.test(telefone));
+	var r = new RegExp(regExp);
+	console.log(r.test(telefone));
 }
